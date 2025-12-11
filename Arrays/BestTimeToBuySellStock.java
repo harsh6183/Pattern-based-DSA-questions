98028ce9998723ac0de543b3810027f318e8d4ac
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-12-11 17:19:22 +0530

    Create BestTimeToBuySellStock.java

---
+++ Arrays/BestTimeToBuySellStock.java
@@ -0,0 +1,21 @@
+//Leetcode 121. Best Time to Buy and Sell Stock
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+class Solution {
+    public int maxProfit(int[] prices) {
+         int minPrices = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minPrices){
+                minPrices=prices[i];
+            }else if(prices[i]-minPrices>maxProfit){
+                maxProfit=prices[i]-minPrices;
+            }
+        }
+        return maxProfit;
+    }
+}
