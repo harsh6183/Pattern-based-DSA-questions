8b8ca6c711c8f07e5c5f27ae7d1b11c4396bcbac
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-12-11 17:49:02 +0530

    Update BuySellStock2.java

--- Arrays/BuySellStock2.java
+++ Arrays/BuySellStock2.java
@@ -1,4 +1,12 @@
 //Leetcode 122. Best Time to Buy and Sell Stock II
+/*Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+*/
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
