a75766b8ce2ae07cc65c6905191a4bd9796044c0
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-12-12 22:26:06 +0530

    Create RomanToInteger.java

---
+++ Arrays/RomanToInteger.java
@@ -0,0 +1,25 @@
+//Leetcode 13. Roman to Integer
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                sum -= value; // subtract if smaller than next
+            } else {
+                sum += value; // otherwise add
+            }
+        }
+        return sum;
+    }
+}
