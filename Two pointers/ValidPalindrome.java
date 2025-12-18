ebdf4bc42ef936ad472085a35b4be3336b440c32
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2025-12-18 22:14:52 +0530

    Create ValidPalindrome.java

---
+++ Two pointers/ValidPalindrome.java
@@ -0,0 +1,32 @@
+//Leetcode 125. Valid Palindrome
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            char ch1 = Character.toLowerCase(s.charAt(i));
+            char ch2 = Character.toLowerCase(s.charAt(j));
+
+            if (ch1 != ch2) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
