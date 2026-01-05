7b802509de49550629e44dd9e5ac6c17d06f5f28
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-01-05 19:17:53 +0530

    Create RansomNote.java

---
+++ Arrays/RansomNote.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length())
+            return false;
+        
+        int[] visited = new int[26];
+
+        for(char ch : magazine.toCharArray()){
+           visited[ch-'a']++;
+        }
+
+        for(char ch : ransomNote.toCharArray()){
+            if( visited[ch-'a'] == 0){
+                return false;
+            }
+            visited[ch-'a']--;
+        }
+        return true;
+    }
+}
+
+        //Using HashMap Time complexity increases
+        // Map<Character,Integer> map = new HashMap<>();
+        
+        // for(int i =0;i<magazine.length();i++){
+        //     if(map.containsKey(magazine.charAt(i))){
+        //         map.put(magazine.charAt(i),map.get(magazine.charAt(i))+1);
+        //     }
+        //     else{
+        //         map.put(magazine.charAt(i),1);
+        //     }
+        // }
+        // int count =0;
+        // for(int i=0;i<ransomNote.length();i++){
+        //     char ch = ransomNote.charAt(i);
+        //     if(map.containsKey(ch)&& map.get(ch)>0){
+        //         count ++;
+        //         map.put(ch,map.get(ch)-1);
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+        // if(count ==ransomNote.length()){
+        //     return true;
+        // }
+        // return false;
