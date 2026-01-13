e556b004d72af38795617dd9591336b6e34e90d6
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-01-13 18:53:59 +0530

    Create RepeatedDNASequences.java

---
+++ Strings/RepeatedDNASequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        //Brute solution
+        Set<String> seen = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        
+        // Time Comp O((n-d).d) Space comp O((n-d).d)
+        for(int i =0;i<=s.length()-10;i++){
+            String dna =  s.substring(i,i+10);
+
+            if(seen.contains(dna)){
+                result.add(dna);
+            }
+            seen.add(dna);
+        }
+        return new ArrayList<String>(result);
+
+
+    }
+}
