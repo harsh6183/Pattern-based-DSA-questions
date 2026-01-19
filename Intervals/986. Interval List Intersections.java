9ad70ef84a26ded777118c719c97de67c2d96129
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-01-19 18:18:38 +0530

    Create 986. Interval List Intersections.java

---
+++ Intervals/986. Interval List Intersections.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> res = new ArrayList<>();
+
+        int i=0;
+        int j=0;
+
+        while(i<firstList.length && j<secondList.length){
+            int e1 = firstList[i][1];//End time of List1
+            int e2 = secondList[j][1];//End time of List2
+            int s1 = firstList[i][0];//Start time of List1
+            int s2 = secondList[j][0];//Start time of List2
+
+            if(e1>=s2 && e2>=s1){
+                res.add(new int[]{
+                    Math.max(s1,s2),
+                    Math.min(e1,e2)
+                });
+            }
+            if(e1<e2){
+                i=i+1;
+            }else{
+                j=j+1;
+            }
+        }
+        int[][] ans= new int[res.size()][2];
+        for(int k=0;k<res.size();k=k+1){
+            ans[k]=res.get(k);
+        }
+        return ans;
+    }
+}
