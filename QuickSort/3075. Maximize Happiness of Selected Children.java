763b1c923a7c8785f79226c11811bcd4a6d8d47a
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-02-18 22:50:27 +0530

    Create 3075. Maximize Happiness of Selected Children.java

---
+++ QuickSort/3075. Maximize Happiness of Selected Children.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+
+        int n = happiness.length;
+        int sum =0;
+        int turn =0; //To minus the (-1) according to turns
+        for(int i=n-1;i>=0;i--){
+            sum=sum+Math.max(happiness[i]-turn,0); //If 0-1 condition comes then take 0
+            turn++;
+            if(turn>=k) break;
+        }
+        return sum;
+    }
+}
