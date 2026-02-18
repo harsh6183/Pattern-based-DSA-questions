51b089cd62147e169057950d1b774cae5d7a45ec
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-02-18 22:58:51 +0530

    Update 3075. Maximize Happiness of Selected Children.java

--- QuickSort/3075. Maximize Happiness of Selected Children.java
+++ QuickSort/3075. Maximize Happiness of Selected Children.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(happiness);
 
         int n = happiness.length;
-        int sum =0;
+        long sum =0; // The error was coming because instead of long i had taken int 
         int turn =0; //To minus the (-1) according to turns
         for(int i=n-1;i>=0;i--){
             sum=sum+Math.max(happiness[i]-turn,0); //If 0-1 condition comes then take 0
