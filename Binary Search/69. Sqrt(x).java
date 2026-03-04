61f4ab6ca16fb44997ed34820c9a042aee7f4816
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-03-04 18:21:26 +0530

    Create 69. Sqrt(x).java

---
+++ Binary Search/69. Sqrt(x).java
@@ -0,0 +1,48 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left =1;
+        int right =x;
+        int result =1;
+
+        if(x==0){
+            return 0;
+        }
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+
+            long sqrt = (long) mid * mid;
+
+            if(sqrt==x){
+                return mid;
+            }else if (sqrt<x){
+                result=mid;
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+class Solution {
+    public int mySqrt(int x) {
+        int start =1;
+        int end =x;
+        while(start <=end){
+            int mid = start +(end-start)/2;
+            if(mid<=x/mid){
+                if(x%mid==0 && mid ==x/mid){
+                    return mid;
+                }
+                start = mid+1;
+            }else {
+                end=mid-1;
+            }
+        }
+        return end;
+    }
+}
