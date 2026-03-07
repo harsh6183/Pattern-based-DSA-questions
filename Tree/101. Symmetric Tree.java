7bb0df0814842abc109d7bc633f7ac3d3c9e6231
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-03-08 00:03:14 +0530

    Update 101. Symmetric Tree.java

--- Tree/101. Symmetric Tree.java
+++ Tree/101. Symmetric Tree.java
@@ -25,14 +25,58 @@ class Solution {
 
         //BFS or leve order traversal
 
-        while(leftTree.isEmpty() && rightTree.isEmpty()){
+        while(!leftTree.isEmpty() && !rightTree.isEmpty()){
             TreeNode leftNode =leftTree.poll();
             TreeNode rightNode =rightTree.poll();
 
             if(leftNode==null && rightNode ==null) continue;
             if(leftNode==null || rightNode ==null) return false;
             if(leftNode.val!= rightNode.val) return false;
+
+            //Pushing order 
+            leftTree.add(leftNode.left);
+            leftTree.add(leftNode.right);
+            rightTree.add(rightNode.right);
+            rightTree.add(rightNode.left);
+
         }
             return true;
+    }
+}
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return tree(root,root);
+    }
+    public boolean tree(TreeNode i,TreeNode k){
+         if(i==null && k==null){
+            return true;
+         }
+         else if (i == null || k == null){
+            return false;
+         }
+         else if(i.val !=k.val){
+            return false;
+         }
+         return tree(i.left,k.right)&& tree(i.right,k.left);
+
     }
 }
