639dbb12acb4a5b3916777540611cc34533597b9
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-03-09 23:08:38 +0530

    Create 117. Populating Next Right Pointers in Each Node II.java

---
+++ Tree/117. Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        //LinkedList approach but level order traversal
+        Node head = root;
+
+        while(head!=null){
+            Node dummy = new Node();
+            Node temp = dummy;
+
+            //Building LL
+            while(head!=null){
+                if(head.left!=null){
+                    temp.next=head.left;
+                    temp=temp.next;
+                }
+                 if(head.right!=null){
+                    temp.next=head.right;
+                    temp=temp.next;
+                }
+                head =head.next;
+            }
+            //Starting from next level
+            head=dummy.next;
+        }
+        return root;
+    }
+}
