97be14b3f577d376514d3514236d244ed343beb3
Harsh Nimbalkar <152949420+harsh6183@users.noreply.github.com>
2026-05-06 23:00:33 +0530

    Update 1004. Max Consecutive Ones III.java

--- Slinding Window/1004. Max Consecutive Ones III.java
+++ Slinding Window/1004. Max Consecutive Ones III.java
@@ -1,1 +1,28 @@
 
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int zeroCounts = 0;
+        int maxOnes = 0;
+        //Time comp O(N) space O(1)
+        //Using sliding window
+        int start = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] == 0) {
+                zeroCounts++;
+            }
+
+            while (zeroCounts > k) {
+                if (nums[start] == 0) {
+                    zeroCounts--;
+                }
+                start++; // always move start
+            }
+
+            // updating AFTER window becomes valid
+            maxOnes = Math.max(maxOnes, end - start + 1);
+        }
+
+        return maxOnes;
+    }
+}
